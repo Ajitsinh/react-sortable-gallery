b13519bac5b2220d9e6a75e3baa25550d5f87e7a
Ajitsinh Solanki <ajitsinh@yahoo.com>
2025-08-08 12:01:47 -0700

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,35 @@ import "react-photo-album/rows.css";
 import SortableGallery from "./components/SortableGallery";
 import photoSet from "./components/photos";
 
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+// import optional lightbox plugins
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+//import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+
+import photos from "./photos";
+
 export default function App() {
   const [photos, setPhotos] = useState(photoSet);
+  const [index, setIndex] = useState(-1);  
 
   return (
+    <>
+      <RowsPhotoAlbum photos={photos} targetRowHeight={150} onClick={({ index }) => setIndex(index)} />
+
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        plugins={[Fullscreen, Slideshow, Zoom]}
+      />
+    </>
+        
     <SortableGallery
       gallery={RowsPhotoAlbum}
       spacing={16}
