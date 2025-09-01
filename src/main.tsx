0619d15d115bb8aec004b66b34f97be02edd2f56
Ajitsinh Solanki <ajitsinh@yahoo.com>
2025-09-01 12:00:07 -0700

    update files

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <header>
       <h1>React Photo Album | Sortable Gallery</h1>
-      <br><p>Drag and drop photos to reorder them in the gallery.</p></br>
+      <br>
+      <p>Drag and drop photos to reorder them in the gallery...</p>
 {/*       <a href="https://github.com/igordanchenko/react-photo-album" target="_blank" rel="noreferrer noopener">GitHub</a>
       <a href="https://react-photo-album.com/" target="_blank" rel="noreferrer noopener">Docs</a> */}
     </header>
